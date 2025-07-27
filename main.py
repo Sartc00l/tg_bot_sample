c2692a030508efb0935574b6bb22a869389002c5
Sartc00l <sukindima025@gmail.com>
2025-07-28 01:53:41 +0300

    end of work
    gg

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import random
 import logging
 import os 
+import sys
 from pathlib import Path
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command
@@ -9,34 +10,26 @@ from aiogram.fsm.state import State, StatesGroup
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove,FSInputFile
 
 
+def get_base_path():
+    if getattr(sys, 'frozen', False):
+        # Если запущен как EXE, путь к временной папке
+        return Path(sys._MEIPASS)
+    else:
+        # Обычный режим (скрипт)
+        return Path(__file__).parent
+print("Rabotaet")
 # Конфигурация
-BOT_TOKEN = "7604459732:AAE49WdblJZh_GyspEfJetWLm-PZUv1vRBI" 
-CHANNEL_ID = "@tgksample"   
+BOT_TOKEN = ""
+CHANNEL_ID = "@zombyaaak_doto"   
 
 # Инициализация объектов
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
 HARDCODED_TEXT = "Гоугоу на стрим!\nhttps://www.twitch.tv/zombyaak_doto"
-HARDCODED_PHOTO_URL_MASSIVE = ['https://i.pinimg.com/736x/cc/34/7d/cc347d12e404f89e0320d40722a81725.jpg',
-                       'https://i.pinimg.com/1200x/4f/eb/17/4feb17bad5cd72657ea01ea0636af713.jpg',
-                       'https://i.pinimg.com/736x/5e/8c/51/5e8c51327816359990e7d9bc9c529ca4.jpg',
-                       'https://i.pinimg.com/736x/c4/92/74/c49274ba06f2bcaee5fe504b43debfa5.jpg',
-                       'https://i.pinimg.com/736x/98/aa/82/98aa82435e568d15da5b44014ee90277.jpg',
-                       'https://i.pinimg.com/736x/2a/c9/e2/2ac9e290f5489685c3f0ee6be622c4a9.jpg',
-                       'https://i.pinimg.com/736x/aa/35/da/aa35daabfaf98d226dfc4fc5837b4ddd.jpg',
-                       'https://i.pinimg.com/736x/a5/23/55/a523557349e5c395c5f9001a5ddb6b81.jpg',
-                       'https://i.pinimg.com/736x/43/fb/8d/43fb8d33570cc10d25fe29449c234722.jpg',
-                       'https://i.pinimg.com/736x/df/e9/07/dfe9073768c1fa153d47a5ac12954407.jpg',
-                       'https://i.pinimg.com/736x/c6/07/91/c607915536f831181a797a916f34a5f7.jpg',
-                       'https://i.pinimg.com/736x/e0/ae/14/e0ae14fb8fef9aeef6b6373985242fa4.jpg',
-                       'https://i.pinimg.com/736x/d6/6f/3c/d66f3c656d061ff8b62ecba19448af8c.jpg',
-                       'https://i.pinimg.com/736x/7f/72/c7/7f72c7ef31a2fc34d55d0d5ea514a242.jpg',
-                       'https://i.pinimg.com/736x/56/44/54/564454677a5a35b28be0ac2b3fd8e2ad.jpg',
-                       'https://i.pinimg.com/736x/f6/39/80/f63980cbd1f3284cce781eee7e050076.jpg',
-                       'https://i.pinimg.com/736x/e9/48/f4/e948f43b8a000b1aae3c6f1e88fdd4cc.jpg',
-                       'https://i.pinimg.com/736x/2a/b0/57/2ab057f4c2b62828c0ad110f77d02628.jpg']
-PHOTO_DIR = Path('C:/Users/eshkere/Documents/python/tg_bots/telegram-zombyak/media/photos')
+
+BASE_DIR = Path(__file__).parent 
+PHOTO_DIR = get_base_path() / "media" / "photos"
 CUSTOM_TEXT = "Гоугоу на стрим!\nhttps://www.twitch.tv/zombyaak_doto"
 
 
@@ -252,3 +245,4 @@ async def main():
 if __name__ == "__main__":
     import asyncio
     asyncio.run(main())
+    print("Rabotaet")
